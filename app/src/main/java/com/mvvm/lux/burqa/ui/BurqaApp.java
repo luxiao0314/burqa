c19c2b7036b81016905aa3e84bfa76f49f12f676
lucio <lucio0314@163.com>
2017-06-12 23:25:21 +0800

    reactActivity优化

--- app/src/main/java/com/mvvm/lux/burqa/ui/BurqaApp.java
+++ app/src/main/java/com/mvvm/lux/burqa/ui/BurqaApp.java
@@ -7,9 +7,16 @@ import android.support.annotation.NonNull;
 import android.support.multidex.MultiDex;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 import com.github.mzule.activityrouter.router.RouterCallback;
 import com.github.mzule.activityrouter.router.RouterCallbackProvider;
 import com.github.mzule.activityrouter.router.SimpleRouterCallback;
+import com.mvvm.lux.burqa.BuildConfig;
 import com.mvvm.lux.burqa.R;
 import com.mvvm.lux.burqa.config.BurqaFrameworkSupport;
 import com.mvvm.lux.burqa.ui.home.activity.LoginActivity;
@@ -18,10 +25,13 @@ import com.mvvm.lux.burqa.ui.sub.activity.NotFoundActivity;
 import com.mvvm.lux.framework.BaseApplication;
 import com.mvvm.lux.framework.config.FrameWorkConfig;
 import com.mvvm.lux.framework.config.session.Session;
+import com.mvvm.lux.framework.http.fresco.ImageLoaderConfig;
 import com.mvvm.lux.framework.utils.SnackbarUtil;
 import com.mvvm.lux.widget.emptyview.LoadingLayout;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,7 +41,7 @@ import java.util.Map;
  * @Date 2017/3/3 14:49
  * @Version
  */
-public class BurqaApp extends BaseApplication implements RouterCallbackProvider {
+public class BurqaApp extends BaseApplication implements RouterCallbackProvider, ReactApplication {
 
     @Override
     public void onCreate() {
@@ -40,6 +50,7 @@ public class BurqaApp extends BaseApplication implements RouterCallbackProvider 
         ARouter.openDebug();
         ARouter.init(this); // 尽可能早，推荐在Application中初始化
         FrameWorkConfig.frameworkSupport = new BurqaFrameworkSupport();
+        SoLoader.init(this, /* native exopackage */ false);
     }
 
     private void initEmptyView() {
@@ -127,4 +138,28 @@ public class BurqaApp extends BaseApplication implements RouterCallbackProvider 
         super.attachBaseContext(base);
         MultiDex.install(this);
     }
+
+    /**
+     * react native相关
+     */
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            //设置rn的图片加载采用android中fresco的,图片采用防盗链处理
+            MainPackageConfig.Builder configBuilder = new MainPackageConfig.Builder();
+            configBuilder.setFrescoConfig(ImageLoaderConfig.getImagePipelineConfig(getAppContext()));
+            return Arrays.<ReactPackage>asList(new MainReactPackage(configBuilder.build()));
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
 }
--- app/src/main/java/com/mvvm/lux/burqa/ui/sub/activity/MyReactActivity.java
+++ app/src/main/java/com/mvvm/lux/burqa/ui/sub/activity/MyReactActivity.java
@@ -2,90 +2,20 @@ package com.mvvm.lux.burqa.ui.sub.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.appcompat.BuildConfig;
-import android.view.KeyEvent;
 
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.common.LifecycleState;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
-import com.facebook.react.shell.MainReactPackage;
-
-public class MyReactActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
-
-    private ReactRootView mReactRootView; 
-    private ReactInstanceManager mReactInstanceManager;
+import com.facebook.react.ReactActivity;
+/**
+ * @Description rn处理activity
+ * @Author luxiao418
+ * @Email luxiao418@pingan.com.cn
+ * @Date 12/06/2017 11:21 PM
+ * @Version 1.0.0
+ */
+public class MyReactActivity extends ReactActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mReactRootView = new ReactRootView(this);
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        // 注意这里的HelloWorld必须对应“index.android.js”中的
-        // “AppRegistry.registerComponent()”的第一个参数
-        mReactRootView.startReactApplication(mReactInstanceManager, "iShiWuPai", null);
-
-        setContentView(mReactRootView);
-    }
-
-    @Override
-    public void invokeDefaultOnBackPressed() {
-        super.onBackPressed();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onHostPause(this);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onHostResume(this, this);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onHostDestroy(this);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onBackPressed();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
+    protected String getMainComponentName() {
+        return "iShiWuPai";
     }
 
     public static void launch(Context context) {
